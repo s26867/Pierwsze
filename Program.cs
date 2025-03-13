37751c02b64cc53918861bf7038f384b599b0ecc
s26867 <s26867@pjwstk.edu.pl>
2025-03-13 13:38:07 +0100

    Dodanie metody na srednia

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("drugi commit");
-Console.WriteLine("Trzeci commit");
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liczby = { 2, 4, 6, 8, 10 };
+            double srednia = ObliczSrednia(liczby);
+            Console.WriteLine("Średnia wynosi: " + srednia);
+        }
+
+        // Metoda obliczająca średnią
+        static double ObliczSrednia(int[] tablica)
+        {
+            if (tablica == null || tablica.Length == 0)
+            {
+                // Opcjonalnie możesz zwrócić 0 lub rzucić wyjątek
+                return 0;
+            }
+
+            double suma = 0;
+            foreach (int liczba in tablica)
+            {
+                suma += liczba;
+            }
+            return suma / tablica.Length;
+        }
+    }
+}
